cfea6326a2576078f40fc352d02d15b9cbfa6ee6
Arian Omidi <arian.omidi@icloud.com>
2021-10-16 16:02:55 -0400

    pending added

--- ECSE428-BACKEND/features/createStudent.steps.js
+++ ECSE428-BACKEND/features/createStudent.steps.js
@@ -30,49 +30,50 @@ defineFeature(feature, (test) => {
 
   test('Create Student Account (Normal Flow)', ({ when, then }) => {
     when(/^the student (.*) wants to create an account with CourseComrade with the password (.*) which matches the verification password (.*) and consists of at least six characters of which at least one is a digit and letter.$/, async (username, password, verif_password) => {
-      const user = await request.post("/createAccount").send({ username, password });
+      pending();
+      // const user = await request.post("/createAccount").send({ username, password });
     });
 
     then(/^a new student account will be created with the username (.*)$/, (arg0) => {
-
+      pending();
     });
 
     then(/^a new user with username (.*) will be populated into the system with the given password (.*)$/, (arg0, arg1) => {
-
+      pending();
     });
   });
 
   test('Attempt to Create a student account with invalid Password format (Error Flow)', ({ when, then }) => {
     when(/^the student (.*) wants to create an account with the password (.*) and a matching verification password (.*)$/, (arg0, arg1, arg2) => {
-
+      pending();
     });
 
     then(/^a resulting "(.*)" message is issued$/, (arg0) => {
-
+      pending();
     });
   });
 
   test('Attempt to Create a student account with password which doesn\'t match the verification password (Error Flow)', ({ when, then }) => {
     when(/^the student (.*) wants to create an account with the password (.*) and a non-matching verification password (.*)$/, (arg0, arg1, arg2) => {
-
+      pending();
     });
 
     then(/^a resulting "(.*)" message is issued$/, (arg0) => {
-
+      pending();
     });
   });
 
   test('Attempt to create a student account with a username which is already associated with an existing account (Error Flow)', ({ given, when, then }) => {
     given(/^student with username (.*) and password (.*) exists$/, (arg0, arg1) => {
-
+      pending();
     });
 
     when(/^a student with username (.*) tries to register an account with a password (.*)$/, (arg0, arg1) => {
-
+      pending();
     });
 
     then(/^a resulting "(.*)" message is issued$/, (arg0) => {
-
+      pending();
     });
   });
 });
--- ECSE428-BACKEND/features/logIn.steps.js
+++ ECSE428-BACKEND/features/logIn.steps.js
@@ -29,11 +29,38 @@ defineFeature(feature, (test) => {
       responseStatus = res.statusCode;
       // responseMessage = res.;
 
-      loggedIn = responseStatus === 666;
+      loggedIn = true;
     });
 
     then('student should not be logged in', () => {
-      expect(loggedIn).toBe(false);
+      expect(true).toBe(false);
+    });
+
+    and(/^student should see the error message "(.*)"$/, (arg0) => {
+      expect(true).toBe(false);
+    });
+  });
+
+
+  test('Attempt to log in with invalid password (Error Flow)', ({ given, when, and, then }) => {
+    given(/^a student account with (.*) and (.*) exists$/, (arg0, arg1) => {
+      pending();
+    });
+
+    when(/^student enters valid username (.*)$/, (arg0) => {
+      pending();
+    });
+
+    and(/^student enters a wrong password (.*)$/, (arg0) => {
+      pending();
+    });
+
+    and('student requests to log in', () => {
+      pending();
+    });
+
+    then('student should be not be logged in', () => {
+      pending();
     });
 
     and(/^student should see the error message "(.*)"$/, (arg0) => {
@@ -42,52 +69,25 @@ defineFeature(feature, (test) => {
   });
 
 
-  test('Attempt to log in with invalid password (Error Flow)', ({ given, when, and, then }) => {
-    given(/^a student account with (.*) and (.*) exists$/, (arg0, arg1) => {
-
-    });
-
-    when(/^student enters valid username (.*)$/, (arg0) => {
-
-    });
-
-    and(/^student enters a wrong password (.*)$/, (arg0) => {
-
-    });
-
-    and('student requests to log in', () => {
-
-    });
-
-    then('student should be not be logged in', () => {
-
-    });
-
-    and(/^student should see the error message "(.*)"$/, (arg0) => {
-
-    });
-  });
-
-
   test('Log in with valid credentials (Normal Flow)', ({ given, when, and, then }) => {
     given(/^a student account with (.*) and (.*) exists$/, (arg0, arg1) => {
-
+      pending();
     });
 
     when(/^student enters a valid username (.*)$/, (arg0) => {
-
+      pending();
     });
 
     and(/^student enters corresponding valid password (.*)$/, (arg0) => {
-
+      pending();
     });
 
     and('student requests to log in', () => {
-
+      pending();
     });
 
     then('student should be successfully logged in', () => {
-
+      pending();
     });
   });
 })
