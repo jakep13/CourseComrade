0bd15866f5b1b226167d4ff8e69144dc00f168b1
Arian Omidi <arian.omidi@icloud.com>
2021-10-16 15:55:54 -0400

    homogenized

--- ECSE428-BACKEND/features/createStudent.steps.js
+++ ECSE428-BACKEND/features/createStudent.steps.js
@@ -27,19 +27,20 @@ const feature = loadFeature(
 
 
 defineFeature(feature, (test) => {
-  test('Create Student Account (Normal Flow)', ({ when, then }) => {
 
+  test('Create Student Account (Normal Flow)', ({ when, then }) => {
     when(/^the student (.*) wants to create an account with CourseComrade with the password (.*) which matches the verification password (.*) and consists of at least six characters of which at least one is a digit and letter.$/, async (username, password, verif_password) => {
       const user = await request.post("/createAccount").send({ username, password });
     });
 
+    then(/^a new student account will be created with the username (.*)$/, (arg0) => {
 
-    then(/^a new user with username (.*) will be populated into the system with the given password (.*)$/, async (arg0, arg1) => {
-      const usr = await User.findOne({ username: arg0, password: arg1 });
-      expect(usr).not.toBeNull();
+    });
+
+    then(/^a new user with username (.*) will be populated into the system with the given password (.*)$/, (arg0, arg1) => {
+
     });
   });
-
 
   test('Attempt to Create a student account with invalid Password format (Error Flow)', ({ when, then }) => {
     when(/^the student (.*) wants to create an account with the password (.*) and a matching verification password (.*)$/, (arg0, arg1, arg2) => {
@@ -51,11 +52,27 @@ defineFeature(feature, (test) => {
     });
   });
 
-
   test('Attempt to Create a student account with password which doesn\'t match the verification password (Error Flow)', ({ when, then }) => {
     when(/^the student (.*) wants to create an account with the password (.*) and a non-matching verification password (.*)$/, (arg0, arg1, arg2) => {
 
     });
+
+    then(/^a resulting "(.*)" message is issued$/, (arg0) => {
+
+    });
   });
 
+  test('Attempt to create a student account with a username which is already associated with an existing account (Error Flow)', ({ given, when, then }) => {
+    given(/^student with username (.*) and password (.*) exists$/, (arg0, arg1) => {
+
+    });
+
+    when(/^a student with username (.*) tries to register an account with a password (.*)$/, (arg0, arg1) => {
+
+    });
+
+    then(/^a resulting "(.*)" message is issued$/, (arg0) => {
+
+    });
+  });
 });
