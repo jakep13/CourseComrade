132bf8e3c827e955db1692e6f89d21f1c0f61840
kitcath <catherine.vangheluwe@mail.mcgill.ca>
2021-10-17 15:43:24 -0400

    dashboard functional component

---
+++ ECSE428-FRONTEND/react-app/src/components/Dashboard Component/Dashboard1.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
+const headerModal = require("react-bootstrap/ModalHeader"); 
+
+function Dashboard1() {
+
+    return (
+        <div>
+            Dashboard 1                  
+        </div>
+    )
+}
+
+export default Dashboard1
