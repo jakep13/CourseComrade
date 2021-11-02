3a268885bfe7fed4af5a859806845d7b12da89db
Cesar Arnouk <arnoukcesar@gmail.com>
2021-11-02 17:12:26 -0400

    edited populate db script

--- ECSE428-BACKEND/populateDB.py
+++ ECSE428-BACKEND/populateDB.py
@@ -13,11 +13,16 @@ courseCollection = db["courseCollection"]
 #     print(user)
 
 course_names = ['ECSE', 'COMP', 'MATH', 'ECON', 'PHIL', 'MGCR', 'FACC', 'FINE', 'PHYS', 'CHEM', 'MECH', 'MIME']
-course_numbers = [i for i in range(100, 599, 10)]
+course_numbers = [i for i in range(100, 599)]
 courses = []
 
 for course_name in course_names:
     for course_number in course_numbers:
         courses.append({'code': course_name + str(course_number)})
 
-courseCollection.insert_many(courses)
+for course in courses:
+    try:
+        courseCollection.insert_one(course)
+    except:
+        print('Course ' + str(course) + ' already in DB')
+        
