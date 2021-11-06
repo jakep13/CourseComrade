0e689d1bcba8225594616dafbacc94465e483fc3
kitcath <catherine.vangheluwe@mail.mcgill.ca>
2021-11-06 14:43:54 -0400

    clean code

--- ECSE428-FRONTEND/react-app/src/components/Search Component/Bar.jsx
+++ ECSE428-FRONTEND/react-app/src/components/Search Component/Bar.jsx
@@ -17,53 +17,10 @@ const config =
 
 
 
-/* to do when backend is done*/
-//const friendResults =[]
 export default function Bar({keyword, setKeyword}) {
-    let [classResults, setClassResults] = useState(0);
-    const [addClasses, setAddClasses] = useState(false);
-    const CloseAddClass = () => setAddClasses(false);
-    const ShowAddClass = () => setAddClasses(true);
-
-    const [myClass, setClass] = useState('');
-
-    
-    function getClassName(className) {
-        console.log(className);
-        setClass(className);
-    }
-
-    function deleteClass(className) {
-        const params = new URLSearchParams()
-        params.append('course', className)
-        //console.log(className)
-
-        axios.post('http://localhost:3100/removeCourse', params, config)
-            .then((result) => {
-            // Redirect
-                console.log(result)
-            })
-            .catch((err) => {
-            })
-    }
-    function addClass(className) {
-        const params = new URLSearchParams()
-        params.append('course', className)
-        //console.log(className)
-
-        axios.post('http://localhost:3100/addCourse', params, config)
-            .then((result) => {
-            // Redirect
-                console.log(result)
-            })
-            .catch((err) => {
-         })
-    }
-
 
     return (
         <div className="searchbar-container">
-            {/* <input className="searchbar" type="search" placeholder='Search a friend or a class' ></input>*/}
             <div className="results-container">
                 <input
                     id="search_bar"
@@ -76,9 +33,6 @@ export default function Bar({keyword, setKeyword}) {
                     onChange={(e) => setKeyword(e.target.value)} 
                 />
                 
- 
-                {/* <Button onClick={() => {addClass(myClass)}}> Add Class </Button>
-                     <Button onClick={() => {deleteClass(myClass)}}> Delete Class</Button>*/}
             </div>
        </div>
     )
